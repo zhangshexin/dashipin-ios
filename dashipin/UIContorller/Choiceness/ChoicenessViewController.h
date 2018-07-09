c93a2bd9b6f9a7aa2594fcf467500df6fddfeb24
zhangshexin <zhangshexin@anne.com.cn>
2018-07-09 15:00:09 +0800

    第一次提交

---
+++ dashipin/UIContorller/Choiceness/ChoicenessViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChoicenessViewController.h
+//  dashipin
+//
+//  Created by zhangshexin on 2018/7/9.
+//  Copyright © 2018年 dashipin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChoicenessViewController : UIViewController
+
+@end
---
+++ dashipin/UIContorller/Mining/MiningViewController.h
@@ -0,0 +1,13 @@
+//
+//  MiningViewController.h
+//  dashipin
+//
+//  Created by zhangshexin on 2018/7/9.
+//  Copyright © 2018年 dashipin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MiningViewController : UIViewController
+
+@end
